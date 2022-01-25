108dcd1f92d40b10fb96933f3a02d85c6950badf
Nelson Neto <nelsonp2n@gmail.com>
2022-01-25 18:04:38 -0300

    Criação dos arquivos de Resource e Collection para Produto para formatação das informações de retorno da API

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'product_id' => $this->product_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'price' => $this->price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
